3844ffafc2077f35cf56d98e858673288e5da911
My Project <example@email.com>
2026-02-27 20:19:13 +0600

    закончил соединять урлы

--- app/settings.py
+++ app/settings.py
@@ -39,7 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+
+    "debug_toolbar",
+
     'main',
+    'goods',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'app.urls'
@@ -103,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-Ru'
 
 TIME_ZONE = 'UTC'
 
@@ -121,6 +127,12 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- app/urls.py
+++ app/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from re import DEBUG
+
 from django.contrib import admin
 from django.urls import include, path
 
@@ -21,5 +23,20 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls', namespace='main') )
+    path('', include('main.urls', namespace='main') ),
+    path('catalog/', include('goods.urls', namespace='catalog') ),
+
 ]
+
+if DEBUG:
+    urlpatterns+=[
+        path('__debug__/', include("debug_toolbar.urls")),
+    ]
+
+"""
+www.site.com/admin/
+www.site.com
+www.site.com/about/
+www.site.com/catalog/
+www.site.com/catalog/product
+"""
